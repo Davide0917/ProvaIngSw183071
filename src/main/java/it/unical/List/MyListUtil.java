feebee8cb1d0714e1797998302a23fe49f1ae092
Davide Pucci <pucci_17@hotmail.com>
2020-11-04 11:57:46 +0100

    Development project with branch

--- src/main/java/it/unical/List/MyListUtil.java
+++ src/main/java/it/unical/List/MyListUtil.java
@@ -1,5 +1,33 @@
 package it.unical.List;
 
+import java.util.List;
+
 public class MyListUtil {
+
+	public List<Integer> setCrescenteList(List<Integer> list) {
+		for (int i = 0; i < list.size(); i++) {
+			for (int j = 0; j < list.size() - 1; j++) {
+				if (list.get(j) > list.get(j + 1)) {
+					int temp = list.get(j);
+					list.set(j, list.get(j + 1));
+					list.set(j + 1, temp);
+				}
+			}
+		}
+		return list;
+	}
+
+	public List<Integer> setDecrescenteList(List<Integer> list) {
+		for (int i = 0; i < list.size(); i++) {
+			for (int j = 0; j < list.size() - 1; j++) {
+				if (list.get(j) < list.get(j + 1)) {
+					int temp = list.get(j);
+					list.set(j, list.get(j + 1));
+					list.set(j + 1, temp);
+				}
+			}
+		}
+		return list;
+	}
 
 }
---
+++ src/test/java/it/unical/List/MyListUtilTest.java
@@ -0,0 +1,48 @@
+package it.unical.List;
+
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+import org.joda.time.*;
+
+public class MyListUtilTest {
+
+	private static MyListUtil listUtil;
+	private static DateTime dt;
+
+	@BeforeClass
+	public static void getAll() {
+		listUtil = new MyListUtil();
+		dt = new DateTime();
+		System.out.println("Time init:" + dt.toString());
+	}
+
+	
+	@Test
+	public void setCrescenteListWorks() {
+		assertEquals(new LinkedList<Integer>(), listUtil.setCrescenteList(new LinkedList<Integer>()));
+		assertEquals(Arrays.asList(1, 5, 9), listUtil.setCrescenteList(Arrays.asList(5, 1, 9)));
+		assertEquals(Arrays.asList(1, 4, 4, 9, 12, 18, 19),
+				listUtil.setCrescenteList(Arrays.asList(4, 1, 9, 19, 18, 12, 4)));
+
+	}
+
+	@Test
+	public void setDecrescenteListWorks() {
+		assertEquals(new LinkedList<Integer>(), listUtil.setDecrescenteList(new LinkedList<Integer>()));
+		assertEquals(Arrays.asList(9, 5, 1), listUtil.setDecrescenteList(Arrays.asList(5, 1, 9)));
+		assertEquals(Arrays.asList(19, 17, 12, 12, 5, 4, 1),
+				listUtil.setDecrescenteList(Arrays.asList(5, 12, 1, 19, 17, 4, 12)));
+
+	}
+
+	@AfterClass
+	public static void closeTime() {
+		dt = new DateTime();
+		System.out.println("Time end: " + dt.toString());
+	}
+}
